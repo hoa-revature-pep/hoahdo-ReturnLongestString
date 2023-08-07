9a6c502dd619f7da8e053da1392ddb99e0d60a01
Hoa Do <hoa.do.ma@gmail.com>
2023-08-07 20:44:50 +0000

    Completed Lab: Return Longest String

--- src/main/java/ReturnLongestString.java
+++ src/main/java/ReturnLongestString.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 public class ReturnLongestString {
     /**
@@ -8,6 +9,17 @@ public class ReturnLongestString {
      * @return the longest String within arr. You can assume that there are no ties for the longest string.
      */
     public String longest(String[] arr){
-        return null;
+
+        HashMap<Integer, String> hMap = new HashMap<>();
+        hMap.put(1, arr[0]);
+
+        for (int idx = 0; idx < arr.length; idx++) {
+
+            if (arr[idx].length() > hMap.get(1).length()) {
+                hMap.put(1, arr[idx]);
+            }
+        }
+
+        return hMap.get(1);
     }
 }
